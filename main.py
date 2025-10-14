17436f92b2c034fb1ec23ea6cc285aba33db1318
Peter Schramm <ptsc4816@hochschule-trier.de>
2025-10-14 18:24:58 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,95 @@
+import click
+import win32pipe
+import win32file
+import pywintypes
+
+MODES = {
+    "inbound": win32pipe.PIPE_ACCESS_INBOUND,
+    "outbound": win32pipe.PIPE_ACCESS_OUTBOUND,
+}
+
+@click.group()
+def cli():
+    """"""
+
+@cli.command("create")
+@click.option("-m", "--mode", type=click.Choice([m for m in MODES], case_sensitive=False), default="inbound", help="Operation mode.")
+@click.argument("name")
+def create(name: str, mode: str):
+    addr = f'\\\\.\\pipe\\{name}'
+    pipe = win32pipe.CreateNamedPipe(
+        addr,
+        MODES[mode],
+        win32pipe.PIPE_TYPE_MESSAGE | win32pipe.PIPE_READMODE_MESSAGE | win32pipe.PIPE_WAIT,
+        1, 65536, 65536,
+        0,
+        None
+    )
+    print(f"created {mode} pipe: {addr}")
+    win32pipe.ConnectNamedPipe(pipe, None)
+    print("got client")
+    try:
+        while True:
+            if mode == "inbound":
+                result, data = win32file.ReadFile(pipe, 64 * 1024)
+                print("Reader received message: " + data.decode())
+            elif mode == "outbound":
+                message = input()
+                win32file.WriteFile(pipe, message.encode())
+    except pywintypes.error as e:
+        if e.winerror == 109:
+            print("Reader pipe closed")
+        else:
+            print(e)
+    finally:
+        win32file.CloseHandle(pipe)
+
+
+@cli.command("read")
+@click.argument("name")
+def read(name: str):
+    addr = f'\\\\.\\pipe\\{name}'
+    try:
+        handle = win32file.CreateFile(
+            addr,
+            win32file.GENERIC_READ,
+            0,
+            None,
+            win32file.OPEN_EXISTING,
+            0,
+            None
+        )
+        while True:
+            result, message = win32file.ReadFile(handle, 64*1024)
+            print(f"message: {message.decode()}")
+    except pywintypes.error as e:
+        if e.winerror == 109:
+            print("Reader pipe closed")
+        else:
+            print(e)
+
+@cli.command("write")
+@click.argument("name")
+def write(name: str):
+    addr = f'\\\\.\\pipe\\{name}'
+    try:
+        handle = win32file.CreateFile(
+            addr,
+            win32file.GENERIC_WRITE,
+            0,
+            None,
+            win32file.OPEN_EXISTING,
+            0,
+            None
+        )
+        while True:
+            message = input()
+            resp = win32file.WriteFile(handle, message.encode())
+    except pywintypes.error as e:
+        if e.winerror == 109:
+            print("Reader pipe closed")
+        else:
+            print(e)
+
+if __name__ == "__main__":
+    cli()
